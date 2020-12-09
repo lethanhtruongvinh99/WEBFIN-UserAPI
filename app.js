ef6e8c0ed8be75746921503370a311af3a159ae2
lethanhtruongvinh99 <lethanhtruongvinh99@gmail.com>
2020-12-09 22:10:46 +0700

    configs cors 2

--- app.js
+++ app.js
@@ -39,6 +39,14 @@ app.set("view engine", "hbs");
 
 app.use(cors());
 app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(passport.initialize());
@@ -63,13 +71,7 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get("env") === "development" ? err : {};
   // render the error page
   res.status(err.status || 500);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
   res.render("error");
-  next();
 });
 //config socket io
 const server = http.createServer(app);
