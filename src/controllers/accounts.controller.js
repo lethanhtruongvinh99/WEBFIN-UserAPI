40f8ecdb99c9daf7cc6d17c1230f2b6873eff2d4
lethanhtruongvinh99 <lethanhtruongvinh99@gmail.com>
2021-01-08 23:15:32 +0700

    invitations

--- src/controllers/accounts.controller.js
+++ src/controllers/accounts.controller.js
@@ -1,5 +1,5 @@
 const Account = require("../models/account");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const BCRYPT_SALT = 10;
 const findAccountByUsername = async (username) => {
   try {
@@ -18,16 +18,16 @@ const findAccountByUsername = async (username) => {
 
 const findAccountById = async (id) => {
   try {
-    const account = await Account.findOne({ _id: id});
+    const account = await Account.findOne({ _id: id });
     if (account) {
-      return {status: true, account: account};
+      return { status: true, account: account };
     } else {
-      return {status: false, account: null};
-    } 
-  } catch(err){
+      return { status: false, account: null };
+    }
+  } catch (err) {
     return err;
   }
-}
+};
 const activateAccount = async (username) => {
   const account = await Account.findOne({ username: username });
   if (account) {
@@ -44,11 +44,11 @@ const activateAccount = async (username) => {
 };
 
 const changeAccountPassword = async (username, newPassword) => {
-  const account = await Account.findOne({username: username});
+  const account = await Account.findOne({ username: username });
   if (account) {
     const newPass = await bcrypt.hash(newPassword, BCRYPT_SALT);
     account.password = newPass;
-    await Account.findOneAndUpdate({username: username}, account);
+    await Account.findOneAndUpdate({ username: username }, account);
     return true;
   } else {
     return false;
@@ -57,10 +57,58 @@ const changeAccountPassword = async (username, newPassword) => {
 
 const getTopPlayer = async () => {
   try {
-    const result = await Account.find().sort({score: -1});
-    return {status: true, result: result};
+    const result = await Account.find().sort({ score: -1 });
+    return { status: true, result: result };
   } catch (err) {
     return err;
   }
-}
-module.exports = { findAccountByUsername, activateAccount, changeAccountPassword, findAccountById, getTopPlayer };
+};
+
+const addInvitation = async (id, senderUsername, roomId, targetUsername) => {
+  try {
+    let targetUser = await Account.find({ username: targetUsername });
+    targetUser.invitations.push({ id: id, username: senderUsername, roomId });
+    const result = await Account.findOneAndUpdate(
+      { username: targetUsername },
+      targetUser
+    );
+    if (result) {
+      return { status: true, data: result };
+    } else {
+      return { status: false, data: "err" };
+    }
+  } catch (err) {
+    return { status: false, data: err };
+  }
+};
+
+const removeInvitation = async (id, senderUsername, roomId, targetUsername) => {
+  try {
+    let targetUser = await Account.find({ username: targetUsername });
+    const findIndex = targetUser.invitations.find((item) => item.id === id);
+    targetUser.invitations = [
+      ...targetUser.invitations.splice(targetUser.invitations(findIndex, 1)),
+    ];
+    targetUser.invitations.push({ id: id, username: senderUsername, roomId });
+    const result = await Account.findOneAndUpdate(
+      { username: targetUsername },
+      targetUser
+    );
+    if (result) {
+      return { status: true, data: result };
+    } else {
+      return { status: false, data: "err" };
+    }
+  } catch (err) {
+    return { status: false, data: err };
+  }
+};
+module.exports = {
+  findAccountByUsername,
+  activateAccount,
+  changeAccountPassword,
+  findAccountById,
+  getTopPlayer,
+  addInvitation,
+  removeInvitation,
+};
